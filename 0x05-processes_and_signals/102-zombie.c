419cc1f77d32520545fa311c9c602ff02e3a4615
jeremy302 <olajeremy123@gmail.com>
2021-10-29 20:12:10 +0100

    fixed bugs

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,4 +1,6 @@
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * infinite_while - an infinite loop
@@ -22,10 +24,13 @@ int infinite_while(void)
  */
 int main(void)
 {
-	int i = 0;
+	int i = 0, pid = 0;
 
-	while (i++ < 5 && fork() != 0)
+	while (i++ < 5 && (pid = fork()) != 0)
+	{
+		printf("Zombie process created, PID: %d\n", pid);
 		if (i == 5)
 			infinite_while();
+	}
 	return (0);
 }
