8e28fbc4487c97fcfdd2028ec1681588043304d2
jeremy302 <olajeremy123@gmail.com>
2022-12-13 17:50:20 +0100

    patch

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,58 +1,59 @@
-# installs flask
-exec { 'apt-get update':
-  path => ['/bin', '/usr/bin'],
+# Nginx server setup and configuration
+exec { 'Update the apt repository':
+  command => 'apt update',
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
 
-package { 'nginx':
-  provider        => apt,
-  install_options => ['-y'],
+package { 'The web server':
+  ensure          => installed,
+  name            => 'nginx',
+  provider        => 'apt',
+  install_options => ['-y']
 }
 
-file { '/etc/nginx/sites-available/alx-1.com':
-  mode    => '0755',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content =>"
-server {
-        listen 80;
-        listen [::]:80;
-
-        root /var/www/alx-1/html;
-        index index.html index.htm index.nginx-debian.html;
-
-        server_name _;
-		rewrite ^/redirect_me / permanent;
-
-        location / {
-                try_files \$uri \$uri/ =404;
-        }
-}
-",
-}
-
-file { '/var/www/alx-1':
-    ensure => 'directory',
-}
-file { '/var/www/alx-1/html':
-    ensure => 'directory',
-}
-
-file { '/var/www/alx-1/html/index.html':
+file { 'The home page':
+  ensure  => file,
+  path    => '/var/www/html/index.html',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
-  content => 'Hello World!',
+  content => "Hello World!\n"
 }
 
-file { '/etc/nginx/sites-enabled/default':
-  ensure => deleted,
+file { 'The 404 page':
+  ensure  => file,
+  path    => '/var/www/error/404.html',
+  mode    => '0744',
+  owner   => 'www-data',
+  content => "Ceci n'est pas une page\n"
 }
 
-exec { 'ln /etc/nginx/sites-available/alx-1.com /etc/nginx/sites-enabled/alx-1.com':
-  path => ['/bin', '/usr/bin'],
+file { 'Nginx server config file':
+  ensure  => file,
+  path    => '/etc/nginx/sites-enabled/default',
+  mode    => '0744',
+  owner   => 'www-data',
+  content =>
+"server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	root /var/www/html;
+	index index.html index.htm index.nginx-debian.html;
+	server_name _;
+	location / {
+		try_files \$uri \$uri/ =404;
+	}
+	if (\$request_filename ~ redirect_me){
+		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
+	}
+	error_page 404 /404.html;
+	location = /404.html {
+		root /var/www/error/;
+		internal;
+	}
+}"
 }
 
 exec { 'Start the server':
   command => 'service nginx restart',
-  path    => '/usr/bin:/usr/sbin:/bin',
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
