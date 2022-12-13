02eb81bdab7033cd83112d31c3cf321aed118fc3
jeremy302 <olajeremy123@gmail.com>
2022-12-13 18:11:34 +0100

    patch

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,4 +1,4 @@
-# Nginx server setup and configuration
+# installs flask
 exec { 'apt-get update':
   path => ['/bin', '/usr/bin'],
 }
@@ -8,47 +8,51 @@ package { 'nginx':
   install_options => ['-y'],
 }
 
-file { 'The home page':
-  ensure  => file,
-  path    => '/var/www/html/index.html',
-  mode    => '0744',
+file { '/etc/nginx/sites-available/alx-1.com':
+  mode    => '0755',
   owner   => 'www-data',
-  content => "Hello World!\n"
+  group   => 'www-data',
+  content =>"
+server {
+        listen 80;
+        listen [::]:80;
+
+        root /var/www/alx-1/html;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+		rewrite ^/redirect_me / permanent;
+
+        location / {
+                try_files \$uri \$uri/ =404;
+        }
+}
+",
 }
 
-file { 'The 404 page':
-  ensure  => file,
-  path    => '/var/www/error/404.html',
-  mode    => '0744',
-  owner   => 'www-data',
-  content => "Ceci n'est pas une page\n"
+file { '/var/www/alx-1':
+    ensure => 'directory',
+}
+file { '/var/www/alx-1/html':
+    ensure => 'directory',
 }
 
-file { 'Nginx server config file':
-  ensure  => file,
-  path    => '/etc/nginx/sites-enabled/default',
+file { '/var/www/alx-1/html/index.html':
   mode    => '0744',
   owner   => 'www-data',
-  content =>
-"server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	root /var/www/html;
-	index index.html index.htm index.nginx-debian.html;
-	server_name _;
-	location / {
-		try_files \$uri \$uri/ =404;
-	}
-	if (\$request_filename ~ redirect_me){
-		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
-	}
-	error_page 404 /404.html;
-	location = /404.html {
-		root /var/www/error/;
-		internal;
-	}
-}"
+  group   => 'www-data',
+  content => "Hello World!\n",
 }
+
+tidy { '/etc/nginx/sites-enabled/default' }
+
+file { '/etc/nginx/sites-available/alx-1.com':
+  ensure => 'link',
+  target => '/etc/nginx/sites-enabled/alx-1.com'
+}
+# exec { 'ln /etc/nginx/sites-available/alx-1.com /etc/nginx/sites-enabled/alx-1.com':
+#   path => ['/bin', '/usr/bin'],
+# }
 
 exec { 'nginx':
   path    => '/usr/bin:/usr/sbin:/bin',
