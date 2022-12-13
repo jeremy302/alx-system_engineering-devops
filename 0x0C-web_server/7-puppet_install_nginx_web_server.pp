094bf8055b10ca0b9b1b7dd66dee2bffa9e01091
jeremy302 <olajeremy123@gmail.com>
2022-12-13 16:37:41 +0100

    puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,57 @@
+# installs flask
+exec { 'apt-get update',
+  path => ['/bin', '/usr/bin'],
+}
+
+package { 'nginx':
+  provider        => apt,
+  install_options => ['-y'],
+}
+
+file { '/etc/nginx/sites-available/alx-1.com':
+  mode    => '0755',n
+  owner   => 'www-data',
+  group   => 'www-data',
+  content =>"
+server {
+        listen 80;
+        listen [::]:80;
+
+        root /var/www/alx-1/html;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+		rewrite ^/redirect_me / permanent;
+
+        location / {
+                try_files \$uri \$uri/ =404;
+        }
+}
+",
+}
+
+file { '/var/www/alx-1':
+    ensure => 'directory',
+}
+file { '/var/www/alx-1/html':
+    ensure => 'directory',
+}
+
+file { '/var/www/alx-1/html/index.html':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'Hello World!',
+}
+
+file { '/etc/nginx/sites-enabled/default':
+  ensure => deleted
+}
+
+exec { 'ln /etc/nginx/sites-available/alx-1.com /etc/nginx/sites-enabled/alx-1.com',
+  path => ['/bin', '/usr/bin'],
+}
+
+exec { 'nginx -s reload':
+  path => ['/usr/sbin'],
+}
