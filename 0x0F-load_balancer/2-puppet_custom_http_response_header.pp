e6020d0c5efd9cfae16f31581ada0b36ea19b544
jeremy302 <olajeremy123@gmail.com>
2022-12-20 04:06:15 +0100

    patch

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,30 +1,36 @@
 # installs and configures nginx with X-Served-By header
 
-exec { '/usr/bin/apt-get update': }
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
+}
 
 package { 'nginx':
   ensure  => installed,
   require => Exec['apt-get-update'],
 }
 
-file_line { 'file1':
+file_line { 'a':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
   line    => 'rewrite ^/redirect_me https://sketchfab.com/bluepeno/models permanent;',
+  require => Package['nginx'],
 }
 
-file_line { 'file2':
+file_line { 'b':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
   line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
 }
 
 file { '/var/www/html/index.html':
-  content => 'Holberton School',
+  content => 'Hello World!',
+  require => Package['nginx'],
 }
 
 service { 'nginx':
   ensure  => running,
+  require => Package['nginx'],
 }
