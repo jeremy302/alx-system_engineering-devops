c087f941e8c58e552f3e0ffeb814aea2e7669a8b
jeremy302 <olajeremy123@gmail.com>
2023-01-16 20:37:24 +0100

    patch

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,7 +6,7 @@ import sys
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
         id = int(sys.argv[1])
         base_url = 'https://jsonplaceholder.typicode.com'
 
