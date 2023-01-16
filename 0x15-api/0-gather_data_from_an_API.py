c200660c52f7a6b6267cd27a6c39555283bf84b3
jeremy302 <olajeremy123@gmail.com>
2023-01-16 17:14:05 +0100

    patch

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,5 +9,16 @@ import urllib.request
 if __name__ == '__main__':
     id = sys.argv[1]
     base_url = 'https://jsonplaceholder.typicode.com'
-    with urllib.request.urlopen("{base_url}/users/{id}") as res:
-         user = json.load(res)
+
+    with urllib.request.urlopen(f"{base_url}/users/{id}") as res:
+        user = json.load(res)
+        user_id = user.get('id')
+    with urllib.request.urlopen("{}/todos?userId={}".format(
+            base_url, user_id)) as res:
+        todos = json.load(res)
+        todos_done = len([t for t in todos if t.get('completed')])
+
+    print("Employee {} is done with tasks({}/{})".format(
+        user.get('name'), todos_done, len(todos)))
+    for t in todos:
+        print('\t {}'.format(t.get('title')))
