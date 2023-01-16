8d54a333c5868d46962e266d6d101df55a0a8c0f
jeremy302 <olajeremy123@gmail.com>
2023-01-16 20:29:38 +0100

    patch

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,16 +6,18 @@ import sys
 
 
 if __name__ == '__main__':
-    id = sys.argv[1]
-    base_url = 'https://jsonplaceholder.typicode.com'
+    if len(sys.argv > 1):
+        id = sys.argv[1]
+        base_url = 'https://jsonplaceholder.typicode.com'
 
-    user = requests.get("{}/users/{}".format(base_url, id)).json()
-    user_id = user.get('id')
-    todos = requests.get("{}/todos?userId={}".format(base_url, user_id)).json()
-    todos_done = len([t for t in todos if t.get('completed')])
+        user = requests.get("{}/users/{}".format(base_url, id)).json()
+        user_id = user.get('id')
 
-    print("Employee {} is done with tasks({}/{})".format(
-        user.get('name'), todos_done, len(todos)))
-    for t in todos:
-        if t.get('completed'):
-            print('\t {}'.format(t.get('title')))
+        todos = requests.get("{}/todos?userId={}".format(
+            base_url, user_id)).json()
+        todos_done = len([t for t in todos if t.get('completed')])
+        print("Employee {} is done with tasks({}/{})".format(
+            user.get('name'), todos_done, len(todos)))
+        for t in todos:
+            if t.get('completed'):
+                print('\t {}'.format(t.get('title')))
