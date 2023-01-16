366c3484161974be459fc530e91f39056934dfbe
jeremy302 <olajeremy123@gmail.com>
2023-01-16 20:26:12 +0100

    patch

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
-'''A script that gathers data from an API.
+'''uses api froom jsonplaceholder
 '''
-import re
 import requests
 import sys
 
 
-API_URL = 'https://jsonplaceholder.typicode.com'
-'''The API's URL.'''
-
-
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
-            todos_res = requests.get('{}/todos'.format(API_URL)).json()
-            user_name = user_res.get('name')
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            todos_done = list(filter(lambda x: x.get('completed'), todos))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    user_name,
-                    len(todos_done),
-                    len(todos)
-                )
-            )
-            for todo_done in todos_done:
-                print('\t {}'.format(todo_done.get('title')))
+    id = sys.argv[1]
+    base_url = 'https://jsonplaceholder.typicode.com'
+
+    user = requests.get("{}/users/{}".format(base_url, id)).json()
+    user_id = user.get('id')
+    todos = requests.get("{}/todos?userId={}".format(base_url, user_id)).json()
+    todos_done = len([t for t in todos if t.get('completed')])
+
+    print("Employee {} is done with tasks({}/{})".format(
+        user.get('name'), todos_done, len(todos)))
+    for t in todos:
+        if t.get('completed'):
+            print('\t {}'.format(t.get('title')))
