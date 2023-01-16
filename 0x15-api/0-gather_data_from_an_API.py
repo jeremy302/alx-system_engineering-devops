dcf9da251c815ed7e5abd765db60936efa1d14eb
jeremy302 <olajeremy123@gmail.com>
2023-01-16 16:00:01 +0100

    t2

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 '''uses api froom jsonplaceholder
 '''
-print('abc')
+
+
+if __name__ == '__main__':
+    id = sys.argv[1]
+    base_url = 'https://jsonplaceholder.typicode.com'
+
+    with urllib.request.urlopen(f"{base_url}/users/{id}") as res:
+        user = json.load(res)
+        user_id = user.get('id')
+    with urllib.request.urlopen(f"{base_url}/todos?userId={user_id}") as res:
+        todos = json.load(res)
+        todos_done = len([t for t in todos if t.get('completed')])
+
+    print("Employee {} is done with tasks({}/{})".format(
+        user.get('name'), todos_done, len(todos)))
+    for t in todos:
+        print('\t {}'.format(t.get('title')))
