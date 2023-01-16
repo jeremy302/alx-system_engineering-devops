0e92fc2ac3ed7bb73962eb22610ff36e155bcad9
jeremy302 <olajeremy123@gmail.com>
2023-01-16 17:10:47 +0100

    patch

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,5 +9,5 @@ import urllib.request
 if __name__ == '__main__':
     id = sys.argv[1]
     base_url = 'https://jsonplaceholder.typicode.com'
-    with urllib.request.urlopen(f"{base_url}/users/{id}") as res:
+    with urllib.request.urlopen("{base_url}/users/{id}") as res:
          user = json.load(res)
