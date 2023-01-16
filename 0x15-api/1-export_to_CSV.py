14df815f71b442dbae77f8ed4ad9d46dce161d64
jeremy302 <olajeremy123@gmail.com>
2023-01-16 21:10:19 +0100

    patch

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 ''' uses api '''
-import sys
 import requests
+import sys
 
 
 if __name__ == '__main__':
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
 ''' uses api '''
 import json
+import requests
 import sys
-import urllib.request
+
 
 if __name__ == '__main__':
-    id = sys.argv[1]
+    id = int(sys.argv[1])
     base_url = 'https://jsonplaceholder.typicode.com'
 
-    with urllib.request.urlopen(f"{base_url}/users/{id}") as res:
-        user = json.load(res)
-        user_id = user['id']
-    with urllib.request.urlopen(f"{base_url}/todos?userId={user_id}") as res:
-        todos = json.load(res)
-
+    user = requests.get("{}/users/{}".format(base_url, id)).json()
+    todos = requests.get("{}/todos".format(base_url)).json()
+    todos = [t for t in todos if t.get('userId') == id]
     arr = []
     for t in todos:
         arr.append({'task': t['title'],
                     'completed': t['completed'],
                     'username': user['username']})
-    data = {str(user_id): arr}
-    with open('{}.json'.format(user_id), 'w') as file:
+    data = {str(id): arr}
+    with open('{}.json'.format(id), 'w') as file:
         json.dump(data, file)
