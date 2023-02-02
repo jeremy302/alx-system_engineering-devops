4288faaadaadd47c0acce52bb565c5d012b4bdba
jeremy302 <olajeremy123@gmail.com>
2023-02-02 05:59:08 +0100

    patch

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,5 @@
 exec { 'patch':
   command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
 /var/www/html/wp-settings.php && service apache2 restart"',
-  path    => ['/usr/sbin', '/bin']
+  path    => '/usr/sbin:/bin:/usr/bin'
 }
