76a8aa33d208e27ed42b473dbe05533d4372eb24
jeremy302 <olajeremy123@gmail.com>
2023-02-09 19:49:23 +0100

    patch

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,4 @@
+# fixes bug on sandbox
 exec { 'patch':
   command => "bash -c \"sed -i.bak 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' /etc/default/nginx && service nginx restart",
   path    => '/usr/bin:/usr/sbin:/bin'
