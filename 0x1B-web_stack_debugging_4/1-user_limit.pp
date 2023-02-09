1433ca7effad01fe698c1d752f2f27ce0993c6ae
jeremy302 <olajeremy123@gmail.com>
2023-02-09 20:24:54 +0100

    patch

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,6 @@
 # fixes bug in sandbox
 exec { 'patch':
-  command => "bash -c \"sed -i.bak 's/^holberton hard nofile 5/holberton hard nofile 88888/' /etc/security/limits.conf && sed -i.bak 's/^holberton soft nofile 4/holberton soft nofile 88888/' /etc/security/limits.conf\"",
+  command => "bash -c \"sed -i.bak 's/^holberton hard nofile 5/holberton hard nofile 88888/' /etc/security/limits.conf && \
+  sed -i.bak 's/^holberton soft nofile 4/holberton soft nofile 88888/' /etc/security/limits.conf\"",
   path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
